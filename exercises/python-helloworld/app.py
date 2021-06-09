c54ecf581b1096d9a2d29ea304942c807aca114d
unknown <richard.jarnen@gmail.com>
2021-06-09 21:33:09 +0800

    add feature: app logging

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,3 +1,5 @@
+import logging
+
 from flask import Flask, json
 
 app = Flask(__name__)
@@ -10,6 +12,8 @@ def status():
         status=200,
         mimetype='application/json'
     )
+    # log line
+    app.logger.info('Status request successful')
 
     return response
 
@@ -21,14 +25,21 @@ def metrics():
         status=200,
         mimetype='application/json'
     )
-
+    app.logger.info('Metrics request successful')
     return response
 
 
 @app.route("/")
 def hello():
+    # log line
+    app.logger.info('Main request successful')
+
     return "Hello World!"
 
 
 if __name__ == "__main__":
+
+    # stream logs to app.log file
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
     app.run(host='0.0.0.0')
